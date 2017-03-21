62cc706a3d4934d62a19f83423f8df0388c02303
Dieter Luypaert <dieterluypaert@gmail.com>
2017-03-21 22:57:51 +0100

    use register-toggle

--- register.js
+++ register.js
@@ -2,52 +2,18 @@
 
 // modules
 
+const register = require('register-toggle');
+
 const millis = require('./index');
-
-// private variables
-
-const functionNames = Object.keys(millis);
-
-const check = {
-  get: () => Number.prototype.BE_MOERIKI_MILLIS_IS_EXTENDED,
-  set() {
-    Number.prototype.BE_MOERIKI_MILLIS_IS_EXTENDED = true;
-  },
-  unset() {
-    delete Number.prototype.BE_MOERIKI_MILLIS_IS_EXTENDED;
-  },
-};
 
 // exports
 
-function register() {
-  if (check.get()) {
-    throw new Error('extending Number twice');
-  }
-
-  functionNames.forEach((modifier) => {
-    if (Number.prototype[modifier]) {
-      throw new Error('cannot overwrite prototye');
-    }
-    Number.prototype[modifier] = function modifyNumber() {
-      return millis[modifier](this);
-    };
-  });
-
-  check.set();
-
-  return reset;
-}
-
-function reset() {
-  if (check.get() === undefined) {
-    return register;
-  }
-  functionNames.forEach((modifier) => {
-    delete Number.prototype[modifier];
-  });
-  check.unset();
-  return register;
-}
-
-module.exports = register();
+module.exports = register({
+  extend: Number.prototype,
+  properties: Object.keys(millis)
+    .reduce((props, modifier) => Object.assign(props, {
+      [modifier]() {
+        return millis[modifier](this);
+      },
+    }), {}),
+});
--- test/register.test.js
+++ test/register.test.js
@@ -1,37 +1,19 @@
-/* eslint no-extend-native:0 */
-
 'use strict';
 
-const reset = require('../register');
+const deregister = require('../register');
 const millis = require('../index');
 
 // tests
 
 describe('register()', () => {
 
-  let register;
-
-  beforeEach(() => {
-    register = reset();
-  });
-
-  it('should throw if already extended', () => {
-    // setup
-    register();
-    // test
-    expect(() => register()).toThrow();
-  });
-
   it('should extend prototype', () => {
-    // setup
-    register();
-    // test & verify
-    expect((1).day()).toBe(Number(millis.day()));
-    expect((1).hour()).toBe(Number(millis.hour()));
-    expect((1).day()).toBe(Number(millis.day()));
-    expect((1).week()).toBe(Number(millis.week()));
-    expect((1).month()).toBe(Number(millis.month()));
-    expect((1).year()).toBe(Number(millis.year()));
+    expect((1).day()).toBe(millis.day());
+    expect((1).hour()).toBe(millis.hour());
+    expect((1).day()).toBe(millis.day());
+    expect((1).week()).toBe(millis.week());
+    expect((1).month()).toBe(millis.month());
+    expect((1).year()).toBe(millis.year());
     expect((2).days()).toBe(2 * millis.days());
     expect((2).hours()).toBe(2 * millis.hours());
     expect((2).days()).toBe(2 * millis.days());
@@ -40,24 +22,6 @@ describe('register()', () => {
     expect((2).years()).toBe(2 * millis.years());
   });
 
-  it('should not remove potentially existing properties when resetting', () => {
-    // setup
-    Number.prototype.days = 'NATIVELY_SET';
-    // test
-    reset();
-    // verify
-    expect(Number.prototype.days).toBe('NATIVELY_SET');
-    // clean up
-    delete Number.prototype.days;
-  });
-
-  it('should throw if property exists', () => {
-    // setup
-    Number.prototype.days = 'ALREADY_SET';
-    // test & verify
-    expect(() => register()).toThrow();
-    // clean up
-    delete Number.prototype.days;
-  });
+  afterAll(deregister);
 
 });
